68bccbb898ad4dae96c08ce4f827479f78ef70c9
3mrotaha <tamr4512@gmail.com>
2022-11-25 22:30:43 +0200

    edited airplane

--- projects/Airport-simulation-with-c/App/privilege.h
+++ projects/Airport-simulation-with-c/App/privilege.h
@@ -1,7 +1,7 @@
 #ifndef	_APP_PRIVILEGE_
 #define _APP_PRIVILEGE_
 #include "app_ErrorStates.h"
-#include "Time/time_config.h"
+#include "../Time/time_config.h"
 
 AppError_t AirPortApplication(void);
 
--- projects/Airport-simulation-with-c/Plane/plane_private.h
+++ projects/Airport-simulation-with-c/Plane/plane_private.h
@@ -1,6 +1,6 @@
 #ifndef _PLANE_PRIV_
 #define _PLANE_PRIV_
-#include "Time/time_config.h"
+#include "../Time/time_config.h"
 
 static Time_t CalculateRunWay(float, int);
 
